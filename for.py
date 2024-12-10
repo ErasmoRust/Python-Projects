961a970f42661e395b8ca2092cc9b422ec07880d
ErasmoRust <erasmo.rust42@gmail.com>
2024-12-10 15:56:24 -0300

    Primeira_versao

---
+++ for.py
@@ -0,0 +1,13 @@
+"""
+numero = int(input('digite um numero: '))
+for variavel in range(numero):
+    print (variavel)
+
+    exemplo de range com um parâmetro
+    """
+
+   #exemplo de range com três parâmetros
+
+for numero in range(1, 12, 3):
+    print (numero)
+
